79386938d86137cd49fb272ad477f0c272a64efb
Akhil Nair <akhil.nair@amey.co.uk>
2017-08-05 21:02:37 +0100

    Update R.sh

--- R.sh
+++ R.sh
@@ -1,9 +1,8 @@
 #/bin/bash
 
 # Install R
-
 . /etc/os-release
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo add-apt-repository 'deb [arch=amd64,i386] https://cran.rstudio.com/bin/linux/ubuntu '$UBUNTU_CODENAME'/'
 sudo apt-get update
-sudo apt-get install -y r-base
+sudo apt-get install -y r-base r-base-dev
